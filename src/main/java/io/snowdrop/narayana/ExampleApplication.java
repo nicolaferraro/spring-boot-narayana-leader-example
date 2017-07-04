0e9e8c0ba7c86f448110bcf20570a38abc0866f3
Nicola Ferraro <ni.ferraro@gmail.com>
2017-07-04 16:25:26 +0200

    Using pod name as node identifier

--- src/main/java/io/snowdrop/narayana/ExampleApplication.java
+++ src/main/java/io/snowdrop/narayana/ExampleApplication.java
@@ -2,12 +2,15 @@ package io.snowdrop.narayana;
 
 import java.util.Collections;
 
+import com.arjuna.ats.arjuna.common.CoreEnvironmentBean;
 import com.arjuna.ats.arjuna.recovery.RecoveryManager;
 import com.arjuna.ats.jbossatx.jta.RecoveryManagerService;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.kubernetes.ha.KubernetesClusterService;
 import org.apache.camel.ha.CamelClusterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jta.narayana.NarayanaRecoveryManagerBean;
@@ -21,7 +24,14 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ExampleApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ExampleApplication.class);
+
     public static void main(String... args) {
+        String nodeIdentifier = System.getenv("NARAYANA_NODE_IDENTIFIER");
+        if (nodeIdentifier != null) {
+            LOG.info("Narayana application using CoreEnvironmentBean.nodeIdentifier={}", nodeIdentifier);
+            System.setProperty("CoreEnvironmentBean.nodeIdentifier", nodeIdentifier);
+        }
         SpringApplication.run(ExampleApplication.class, args);
     }
 
